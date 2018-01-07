ea81360e97f737955ca8f2b4adb81a2fb217df55
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-07 16:36:12 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,56 @@
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+];
+  
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+];
+
+var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+
+var tvShows = ["game of thrones", "true detective", "the good wife", "empire"];
+
+var weirdGreeting = ["he", 110, "w", 0, "r", 1, "d"];
+
+var empty = [ ];
+
+var evenNumbers = new Array();
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var superheroines = ["catwoman", "she-hulk", "mystique"];
+
+superheroines.push("wonder woman");
+
+var cities = ["New York", "San Francisco"]
+
+cities = ["Philadelphia", ...cities]
+
+function addElementToBeginningOfArray(array, element) {
+  newArray = array
+  newArray.unshift(element)
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function accessElementinArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
